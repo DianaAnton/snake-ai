c473be5c6e8940b25b1a9c63caa3af03b80e8a15
Diana Anton <diana.anton00@e-uvt.ro>
2021-12-30 12:42:00 +0200

    snake-ai game

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # snake-ai
+### Training AI to play Snake using Reinforcement Learning 
+# 
+## Description
+This is a small project that was done following this playlist from youtube [Teach AI To Play Snake! Reinforcement Learning](https://www.youtube.com/playlist?list=PLqnslRFeH2UrDh7vUmJ60YrmWd64mTTKV).
+
+All credits go to [Patrick Loeber (Python Engineer)](https://github.com/python-engineer).
---
+++ src/model.py
@@ -0,0 +1,71 @@
+import torch, os
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+#==============================================================================#
+class Linear_QNet(nn.Module):
+#------------------------------------------------------------------------------#
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.linear1 = nn.Linear(input_size, hidden_size)
+        self.linear2 = nn.Linear(hidden_size, output_size)
+#------------------------------------------------------------------------------#
+    def forward(self, x):
+        x = F.relu(self.linear1(x))
+        x = self.linear2(x)
+        return x
+#------------------------------------------------------------------------------#
+    def save(self, file_name='model.pth'):
+        model_folder_path = './model'
+        if not os.path.exists(model_folder_path):
+            os.makedirs(model_folder_path)
+        file_name = os.path.join(model_folder_path, file_name)
+        torch.save(self.state_dict(), file_name)
+#==============================================================================#
+class QTrainer:
+#------------------------------------------------------------------------------#
+    def __init__(self, model, learning_rate, gamma):
+        self.model = model
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.optimizer = optim.Adam(model.parameters(), lr = self.learning_rate)
+        self.criterion = nn.MSELoss()
+#------------------------------------------------------------------------------#
+    def train_step(self, state, action, reward, next_state, done):
+        # convert to tensor
+        state = torch.tensor(state, dtype = torch.float)
+        next_state = torch.tensor(next_state, dtype = torch.float)
+        action = torch.tensor(action, dtype = torch.long)
+        reward = torch.tensor(reward, dtype = torch.float)
+        # (n, x) if it has multiple values
+
+        if len(state.shape) == 1:
+            # one dimension, one number
+            # convert to format (1, x), tuples
+            state = torch.unsqueeze(state, 0)
+            next_state = torch.unsqueeze(next_state, 0)
+            action = torch.unsqueeze(action, 0)
+            reward = torch.unsqueeze(reward, 0)
+            done = (done, )
+
+        # 1: predicted Q values with the current state
+        prediction = self.model(state)
+
+        # 2: Q_new = reward + gamma * max(next_predicted Q value) -> only do this if not done
+        # prediction.clone()
+        target = prediction.clone()
+        # predictions[argmax(action)] = Q_new
+        for index in range(len(done)):
+            Q_new = reward[index]
+            if not done[index]:
+                Q_new = reward[index] + self.gamma * torch.max(self.model(next_state[index]))
+            target[index][torch.argmax(action).item()] = Q_new
+
+        # apply loss function
+        self.optimizer.zero_grad()
+        loss = self.criterion(target, prediction)
+        # backward propagation
+        loss.backward()
+        self.optimizer.step()
+#------------------------------------------------------------------------------#
+#==============================================================================#
